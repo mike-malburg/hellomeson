281a920c4b63c5203ab5e2ff6a353802b7e7cfc2
Mike Malburg <m.malburg@techsmith.com>
2024-10-30 13:41:10 -0400

    Print osmacros in hellomesontest

--- test/hellomesontest.cpp
+++ test/hellomesontest.cpp
@@ -1,11 +1,15 @@
-#include "hellomeson.h"
 #include <iostream>
+#include "hellomeson.h"
+#include "osmacros.h"
 
-int main()
-{
+int main() {
    std::cout << "Testing hellomeson..." << std::endl;
-   auto hello = std::make_unique<hellomesonlib::hellomeson>();
+   const auto hello = std::make_unique<hellomesonlib::hellomeson>();
    std::cout << "hellomeson::getHello() = " << hello->getHello() << std::endl;
    std::cout << "hellomeson::getWorld() = " << hello->getWorld() << std::endl;
+
+   std::cout << std::endl;
+   std::cout << "Printing OSVersion definitions..." << std::endl;
+   osmacros::print();
    return 0;
 }
